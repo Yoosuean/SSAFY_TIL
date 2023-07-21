8f24005f7153e72950891e4aeb6a4159018df526
Yoosuean <awetumnn@gmail.com>
2023-07-21 23:57:03 +0900

    230721

---
+++ SSAFY_ALGORITHM_JAVA/1592.java
@@ -0,0 +1,32 @@
+package SSAFY_ALGORITHM_JAVA;
+
+import java.util.*;
+
+class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int m = scan.nextInt();
+		int l = scan.nextInt();
+		int[] arr = new int[n];
+		int answer = 0;
+		int temp = 0;
+		while(true) {
+			if(arr[temp] % 2 == 1) {
+				temp = (temp + l) % n;
+				arr[temp]++;
+				answer++;
+			}
+			else if(arr[temp] % 2 == 0) {
+				temp = ((temp - l) + n) % n;
+				arr[temp]++;
+				answer++;
+			}
+			if(arr[temp] == m)
+				break;
+		}
+        --answer;
+		System.out.println(answer);
+	}
+}
