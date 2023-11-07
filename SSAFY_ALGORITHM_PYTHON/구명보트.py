565786d5053b05d38ce3cde0a15157b08e18819f
Suean Yoo <74358436+Yoosuean@users.noreply.github.com>
2023-11-07 17:17:03 +0900

    [프로그래머스/LV2] 구명보트

---
+++ SSAFY_ALGORITHM_PYTHON/구명보트.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+def solution(people, limit):
+    people.sort()
+    q=deque(people)
+    cnt=0
+    
+    while q:
+        if len(q)==1:
+            cnt+=1
+            break
+        if q[0]+q[-1]<=limit:
+            q.popleft()
+            q.pop()
+            cnt+=1
+        else:
+            q.pop()
+            cnt+=1
+            
+    return cnt
