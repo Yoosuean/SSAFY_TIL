1f9eeabd03d3c0ca0bbb8c3534c485f342b3b16c
Suean Yoo <74358436+Yoosuean@users.noreply.github.com>
2023-11-11 21:42:41 +0900

    [프로그래

---
+++ SSAFY_ALGORITHM_PYTHON/올바른괄호.py
@@ -0,0 +1,13 @@
+def solution(s):
+    temp_arr=[]
+    
+    for i in s:
+        if i=='(':
+            temp_arr.append('(')
+        else:
+            if(len(temp_arr)>0):
+                temp_arr.pop()
+            else:
+                return False
+    if len(temp_arr)==0: return True
+    return False
