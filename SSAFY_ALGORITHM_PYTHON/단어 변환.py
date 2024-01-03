63efb91f7cab5f86fc2861f5b9b8b170b9cfe0ec
Suean Yoo <74358436+Yoosuean@users.noreply.github.com>
2024-01-03 11:04:21 +0900

    단어 변환.py

--- SSAFY_ALGORITHM_PYTHON/단어 변환.py
+++ SSAFY_ALGORITHM_PYTHON/단어 변환.py
@@ -4,9 +4,9 @@ def solution(begin, target, words):
     global min_res
     w_len=len(words)
     checked=[False for _ in range(w_len)]
-    
+
     dfs(begin,target,words,0,w_len,checked)
-    
+
     if min_res==INF:
         min_res=0
     return min_res
@@ -14,10 +14,9 @@ def solution(begin, target, words):
 def dfs(begin,target,words,cnt,w_len,checked):
     global min_res
     if begin==target:
-            print(cnt)
             min_res=min(cnt,min_res)
             return
-        
+
     for word in range(len(words)):
         if checked[word]:
             continue
@@ -26,9 +25,7 @@ def dfs(begin,target,words,cnt,w_len,checked):
             temp=words[word]
             if temp[i]==begin[i]:
                 same+=1
-        if same==2:
+        if same==len(words[0])-1:
             checked[word]=True
             dfs(words[word],target,words,cnt+1,w_len,checked)
             checked[word]=False
-        
-    
